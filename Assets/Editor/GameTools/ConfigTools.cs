edcfad0315daa164949ea8fef1e8824c1f2ad09c
E_Ye <E_Ye@outlook.com>
2019-05-28 23:07:40 +0800

    添加自动生成配置表获取方法编辑器功能，支持自动匹配生成区间覆盖对应区域，不影响自定义方法ConfigCfgManager

--- Assets/Editor/GameTools/ConfigTools.cs
+++ Assets/Editor/GameTools/ConfigTools.cs
@@ -2,13 +2,14 @@
 using UnityEditor;
 using System.IO;
 using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using Debug = UnityEngine.Debug;
 
 /// <summary>
-/// modify by zfc @ 2018.11.16
+/// modify by E_Ye @ 2019.5.28
 /// 说明：此处xlsx生成lua 以及proto 生成lua配置工具
-/// 如果生成失败 配置下protobuf环境 已经python环境 备注：python版本最好是2 以及安装读取excel库 xlrd
 /// </summary>
-
 public class ConfigTools : EditorWindow
 {
     private static string _luaOutPutFolder = string.Empty;
@@ -20,9 +21,12 @@ public class ConfigTools : EditorWindow
 
     private bool foldOutClick;
     private string genBatName = "start.bat";
-    
+
+
     void OnEnable()
     {
+        maxSize = new Vector2(600, 320);
+//        minSize = maxSize;
         ReadPath();
     }
 
@@ -36,29 +40,31 @@ public class ConfigTools : EditorWindow
     private void OnGUI()
     {
         GUILayout.Space(10);
-        
+
         GUILayout.BeginHorizontal();
-        GUILayout.Label("Tools path : ", EditorStyles.boldLabel, GUILayout.Width(150));
-        _toolRootPath = GUILayout.TextField(_toolRootPath, GUILayout.Width(240));
-        if (GUILayout.Button("...", GUILayout.Width(40)))
+        GUILayout.Label("Generate tools path : ", EditorStyles.boldLabel, GUILayout.Width(150));
+        _toolRootPath = GUILayout.TextField(_toolRootPath, GUILayout.Width(400));
+        if (GUILayout.Button("...", GUILayout.Width(30)))
         {
             SelectToolFolder();
         }
+
         GUILayout.EndHorizontal();
 
         GUILayout.Space(10);
-        
+
         GUILayout.BeginHorizontal();
         GUILayout.Label("Lua output path : ", EditorStyles.boldLabel, GUILayout.Width(150));
-        _luaOutPutFolder = GUILayout.TextField(_luaOutPutFolder, GUILayout.Width(240));
-        if (GUILayout.Button("...", GUILayout.Width(40)))
+        _luaOutPutFolder = GUILayout.TextField(_luaOutPutFolder, GUILayout.Width(400));
+        if (GUILayout.Button("...", GUILayout.Width(30)))
         {
             SelectOutputFolder();
         }
+
         GUILayout.EndHorizontal();
-        
+
         GUILayout.Space(10);
-        
+
         //协议生成暂时不用
 //        GUILayout.BeginHorizontal();
 //        GUILayout.Label("proto path : ", EditorStyles.boldLabel, GUILayout.Width(80));
@@ -69,21 +75,33 @@ public class ConfigTools : EditorWindow
 //        }
 //        GUILayout.EndHorizontal();
 
+        GUILayout.BeginVertical();
         GUILayout.BeginHorizontal();
-//        GUILayout.Label("---------------------");
-        if (GUILayout.Button("xlsx gen lua", GUILayout.Width(100)))
+        GUILayout.Space(100);
+        if (GUILayout.Button("Xlsx Gen Lua", GUILayout.Width(150)))
         {
             XlsxGenLua();
         }
-//        GUILayout.Label("---------------------");
+        GUILayout.Space(100);
+        if (GUILayout.Button("Gen ConfigCfgManager", GUILayout.Width(150)))
+        {
+            Gen2ConfigCfgManager();
+        }
+
+        
         GUILayout.EndHorizontal();
         
         foldOutClick = EditorGUILayout.Foldout(foldOutClick, "帮助"); // 定义折叠菜单
         if (foldOutClick)
         {
             EditorGUILayout.HelpBox("上方设置的路径为生成配置lua文件的存放,根据项目自身目录设置！", MessageType.Info); // 显示一个提示框
+            EditorGUILayout.HelpBox("Gen ConfigCfgManager功能自动遍历lua配置表路径下的lua文件生成", MessageType.Info); // 显示一个提示框
         }
         
+        GUILayout.EndVertical();
+
+        
+
 //        协议生成留着以后
 //        GUILayout.Space(20);
 //        GUILayout.BeginHorizontal();
@@ -100,49 +118,73 @@ public class ConfigTools : EditorWindow
     {
         Process p = new Process();
         p.StartInfo.WorkingDirectory = _toolRootPath;
-        p.StartInfo.FileName = _toolRootPath+"/"+genBatName;
+        p.StartInfo.FileName = _toolRootPath + "/" + genBatName;
         p.StartInfo.Arguments = _luaOutPutFolder;
         p.StartInfo.UseShellExecute = false;
         p.StartInfo.CreateNoWindow = false;
         p.Start();
     }
 
-//    private void XlsxGenLua()
-//    {
-//        if (!CheckXlsxPath(luaOutPutFolder))
-//        {
-//            return;
-//        }
-//
-//        Process p = new Process();
-//        p.StartInfo.FileName = @"python";
-//        p.StartInfo.Arguments = luaOutPutFolder + "/tools/toconfigs.py";
-//        p.StartInfo.UseShellExecute = false;
-//        p.StartInfo.RedirectStandardOutput = true;
-//        p.StartInfo.RedirectStandardInput = true;
-//        p.StartInfo.RedirectStandardError = true;
-//        p.StartInfo.CreateNoWindow = true;
-//        p.StartInfo.WorkingDirectory = luaOutPutFolder + "/tools";
-//        p.Start();
-//        p.BeginOutputReadLine();
-//        p.OutputDataReceived += new DataReceivedEventHandler((object sender, DataReceivedEventArgs e) =>
-//        {
-//            if (!string.IsNullOrEmpty(e.Data))
-//            {
-//                UnityEngine.Debug.Log(e.Data);
-//                if (e.Data.Contains("SUCCEEDED"))
-//                {
-//                    Process pr = sender as Process;
-//                    if (pr != null)
-//                    {
-//                        pr.Close();
-//                    }
-//                    xlsxGenLuaFinished = true;
-//                }
-//            }
-//        });
-//    }
-    
+    /// <summary>
+    /// 生成ConfigCfgManager获取配置接口
+    /// </summary>
+    private void Gen2ConfigCfgManager()
+    {
+        Logger.LogColor(Color.green,  ">>>Generated ConfigCfgManager start ");
+        StringBuilder sb = new StringBuilder();
+        string mgrContent = null;
+        string machContent = null;
+        string configMgrPath = string.Format("{0}/../ConfigCfgManager.lua", _luaOutPutFolder);
+        if (string.IsNullOrEmpty(_luaOutPutFolder))
+        {
+            Debug.LogError("路径为空，请选择lua路径");
+            return;
+        }
+
+        var luaFiles = UtilEditorUI.FindFiles(_luaOutPutFolder, "*.lua", true);
+        if (luaFiles.Length > 0)
+        {
+            string genPatt = @"---Auto Generated Start---([\s\S]*).*?---Auto Generated End---";
+            mgrContent = File.ReadAllText(configMgrPath, Encoding.Default);
+            Match mc = Regex.Match(mgrContent, genPatt);
+            machContent = mc.Value;
+            Debug.LogError(mc.Value);
+        }
+
+        if (string.IsNullOrEmpty(machContent))
+        {
+            Debug.LogError(">>>>>找不到匹配的---Generated start--- 生成区块， 本次生成ConfigCfgManager失败，请检查");
+            return;
+        }
+
+        sb.Clear();
+        sb.AppendLine("---Auto Generated Start---");
+        foreach (var luaFile in luaFiles)
+        {
+            var tempFileName = luaFile.Replace(@"\","");
+            tempFileName = tempFileName.Replace(".lua", "");
+
+            GenEveryFileFromName(ref sb, tempFileName);
+        }
+        sb.AppendLine("---Auto Generated End---");
+        mgrContent = mgrContent.Replace(machContent, sb.ToString());
+        
+        File.WriteAllText(configMgrPath, mgrContent, Encoding.Default);
+        Logger.LogColor(Color.green,  ">>>Generated ConfigCfgManager finish !!");
+    }
+
+    private void GenEveryFileFromName(ref StringBuilder sb, string fileName)
+    {
+        string helperName = fileName.Replace("Table", "Helper");
+        string functionNmae = helperName.Replace("Helper", "");
+        functionNmae = functionNmae.Replace("Cfg", "Get");
+
+        sb.AppendFormat("\n---@return {0}\n", helperName);
+        sb.AppendFormat("function ConfigCfgManager:{0}()\n", functionNmae);
+        sb.AppendFormat("\treturn reload('Config.Data.{0}')\n", fileName);
+        sb.AppendFormat("end\n");
+    }
+
     private void ProtoGenLua()
     {
         if (!CheckProtoPath(protoFolder))
@@ -173,12 +215,13 @@ public class ConfigTools : EditorWindow
                     {
                         pr.Close();
                     }
+
                     protoGenLuaFinished = true;
                 }
             }
         });
     }
-    
+
     void Update()
     {
         if (protoGenLuaFinished)
@@ -211,7 +254,7 @@ public class ConfigTools : EditorWindow
         _luaOutPutFolder = outoutPath;
         SavePath();
     }
-    
+
     /// <summary>
     /// 选择生成工具目录
     /// </summary>
